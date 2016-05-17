530bf20f9e01f459bf207fe148c12fde4cb481ef
Jonathan Blair <jonathan@Jonathans-MacBook-Pro.local>
2016-05-17 13:22:37 -0400

    ?

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,27 @@
 'use strict';
 
 var $ = require('jquery');
+var validate = require('jquery-validation');
+
+window.$ = window.jQuery = $;
+var bootstrap = require('bootstrap-sass');
+
+var formHelpers = {
+  sendButtonResponse: function(btnEl, success) {
+    var delay = btnEl.hasClass('send-clicked') ? 1500 : 0
+      , noSendDelay = 2000;
+
+    setTimeout(function() {
+      btnEl.removeClass('send-clicked' );
+      btnEl.addClass(success ? 'send-success' : 'send-fail');
+    }, delay);
+
+
+    setTimeout(function() {
+      btnEl.removeClass('send-fail');
+    }, delay + noSendDelay);
+  }
+};
 
 var backgroundIconArray = []
   , depthElementArray = []
@@ -61,7 +82,83 @@ function animateBackgroundIcons() {
   }, 1000);
 }
 
+$('#contact-form').submit(function(e) {
+  e.preventDefault();
+});
+
 $('document').ready(function() {
+  $('#contact-form').validate({
+      rules: {
+        name: 'required',
+        email: {
+          required: true,
+          email: true
+        },
+        message: 'required'
+      },
+      messages: {
+        name: 'Who are you?',
+        email: 'Something is up with your email',
+        message: 'You gotta say something'
+      },
+      submitHandler: function(form) {
+        var contactForm = $(form)
+          , baseUrl = contactForm.attr('action')
+          , name = contactForm.find('#contact-name')
+          , email = contactForm.find('#contact-email')
+          , phone = contactForm.find('#contact-phone')
+          , message = contactForm.find('#contact-message')
+          , sendBtn = contactForm.find(':submit');
+        console.log('Hello:', baseUrl);
+        if(sendBtn.hasClass('send-success' || sendBtn.hasClass('send-clicked'))) {
+          return true;
+        }
+
+        sendBtn.addClass('send-clicked');
+
+        $.ajax({
+          url: baseUrl,
+          data: {
+            'name': name.val(),
+            'email': email.val(),
+            'phone': phone.val(),
+            'message': message.val()
+          },
+          type: 'POST',
+          dataType: 'xml',
+          success: function() {
+            name.val('');
+            email.val('');
+            phone.val('');
+            message.val('');
+
+            formHelpers.sendButtonResponse(sendBtn, true);
+          },
+          error: function(xhr, textStatus, errorThrown){
+            formHelpers.sendButtonResponse(sendBtn, true);
+          }
+        });
+      },
+      invalidHandler: function(form, validator) {
+        formHelpers.sendButtonResponse($(form.target).find(':submit'), false);
+      },
+      errorElement: "span",
+      errorClass: "help-block",
+      highlight: function(element, errorClass, validClass) {
+        $(element).closest('.form-group').addClass('has-error');
+      },
+      unhighlight: function(element, errorClass, validClass) {
+        $(element).closest('.form-group').removeClass('has-error');
+      },
+      errorPlacement: function(error, element) {
+        if (element.parent('.input-group').length || element.prop('type') === 'checkbox' || element.prop('type') === 'radio') {
+          error.insertAfter(element.parent());
+        } else {
+          error.insertAfter(element);
+        }
+      }
+    });
+
   depthElementArray = $('*[data-depth]');
 
   for (var i = 0; i < depthElementArray.length; i++) {
